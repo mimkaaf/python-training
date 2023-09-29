69314a8f9fc82cd2bbd1cd03c4e89ae48a22ebf3
Maysam Kashipour <maysamkashipour@gmail.com>
2023-09-29 10:47:40 -0400

    Getting input from users

---
+++ GettingInputFromUsers.py
@@ -0,0 +1,6 @@
+# using input function to get input from users
+# the input is always a string
+# we may need to convert format (even if they enter a number)
+userName = input("Enter your name: ")
+lenUserName = len(userName)
+print(lenUserName)
