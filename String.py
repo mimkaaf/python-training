08cf14f6ca473c4f7af07eb027cf98ad32661c02
Maysam Kashipour <maysamkashipour@gmail.com>
2023-09-29 10:42:47 -0400

    How to use string in python

---
+++ String.py
@@ -0,0 +1,4 @@
+# store this address as a string in a variable C:\user\docs\Letter.txt
+# print "The file is saved under <address>"
+address = "C:"+"\\"+"user"+"\\"+"docs"+"\\"+"Letter.txt"
+print(f"The file is saved under {address}") 
