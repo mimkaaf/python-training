10c293d8e4d9438fae02993b1e446a4b9855ded7
mimkaaf <146407717+mimkaaf@users.noreply.github.com>
2023-10-01 19:58:51 -0400

    Lists and Slice

---
+++ DataType.py
@@ -0,0 +1,1 @@
+# Write a program to store students' names and whether they are present at the class (understanding data structure)
---
+++ ListsAndSlice.py
@@ -0,0 +1,6 @@
+myList = [1, 2.3, "Maysam", True]
+print(myList[1:3])
+print(myList[:3])
+print(myList[1:])
+print("Ali" in myList)
+Print("Ali" not in myList)
