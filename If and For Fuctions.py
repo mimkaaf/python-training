a41ca625f8945cc50bac24b45fe232da560e2b32
Maysam Kashipour <maysamkashipour@gmail.com>
2023-10-05 21:19:19 -0400

    If and For Fuctions

---
+++ If and For Fuctions.py
@@ -0,0 +1,9 @@
+# If and For Fuctions
+cities = ['New York', 'Washington', 'Miami', 'Tehran']
+if cities[0] == 'Tehran':
+    print("Tehran is the first city")
+else:
+    print("Bummer!")
+for city in cities:
+    print(city)
+print('Done!')
