aa3ab3248abd67d0f0a7ff2cc15b8a5d91a4f0f1
Maysam Kashipour <maysamkashipour@gmail.com>
2023-10-06 21:34:53 -0400

    adding For over dictionary

--- If and For Fuctions.py
+++ If and For Fuctions.py
@@ -7,3 +7,13 @@ else:
 for city in cities:
     print(city)
 print('Done!')
+
+# Iterate over a dictionary
+dictionary = {
+    'New York' : 'Big',
+    'Tehran' : 'Big',
+    'Providence' : 'Small'
+}
+for key, value in dictionary.items():
+    print("This city is {} and it is {}".format(key, value))
+
